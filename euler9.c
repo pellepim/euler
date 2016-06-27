efc388b2b0cd4be208c250e780bf99a0acbf1ea1
Jon Nylander <pellepim@gmail.com>
2016-06-27 22:56:58 +0200

    little more docs

--- euler9.c
+++ euler9.c
@@ -2,27 +2,37 @@
 #include <stdlib.h>
 #include <math.h>
 
-/**
- * Lets try to find some boundries to the problem first, in order to make it slightly
- * less "brute-forceish".
- *
- * The given constraints are the a < b < c and that a + b + c = 1000.
- *
- * HYPOTHESIS 1: c <= 500.
- * PROOF: Assume that c > 500. That would mean that a + b < 500, for each given b
- * it would have to hold that b > a and a >= 1 in order for the original constraints to hold.
- * Assuming the lowest possible value for a (1) gives that the maximum possible value for b
- * is 499. Therefore it is impossible for c to be larger than 500.
- *
- * HYPOTHESIS 2: c >= 334
- * PROOF: Assume that c < 334. Then it would have to hold that a + b = 666. Assuming the lowest
- * possible value for a (1) would mean that b would have to be 665, invalidating the given
- * constaint of c > b. To find the minimum possible value of c that leaves enough "space" for
- * b to be larger than a while still being lower than c i 666/2 + 1. E.g 334.
- *
- * These two proofs are represented below as c_upper_boundry and c_lower_boundry
+/*
+ Problem 9:
+ A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ 
+ a^2 + b^2 = c^2
+ For example, 32 + 42 = 9 + 16 = 25 = 52.
+ 
+ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ 
+ Find the product abc.
+ 
+ Solution: 
+ Lets try to find some boundries to the problem first, in order to make it slightly
+ less "brute-forceish".
+ 
+ The given constraints are the a < b < c and that a + b + c = 1000.
+ 
+ HYPOTHESIS 1: c <= 500.
+ PROOF: Assume that c > 500. That would mean that a + b < 500, for each given b
+ it would have to hold that b > a and a >= 1 in order for the original constraints to hold.
+ Assuming the lowest possible value for a (1) gives that the maximum possible value for b
+ is 499. Therefore it is impossible for c to be larger than 500.
+ 
+ HYPOTHESIS 2: c >= 334
+ PROOF: Assume that c < 334. Then it would have to hold that a + b = 666. Assuming the lowest
+ possible value for a (1) would mean that b would have to be 665, invalidating the given
+ constaint of c > b. To find the minimum possible value of c that leaves enough "space" for
+ b to be larger than a while still being lower than c i 666/2 + 1. E.g 334.
+ 
+ These two proofs are represented below as c_upper_boundry and c_lower_boundry
  */
-
 int main() {
     int a;
     int comparisons = 0;
